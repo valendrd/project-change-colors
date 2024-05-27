e5d08fa0b2c34c8ef4860ce83eb4dfda70a0a397
valen crego <valentincrego@gmail.com>
2024-05-27 20:43:38 -0300

    primer proyecto

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const changeColorfdc437 = () => {
+    document.getElementById("lorem").style.color="#fdc437"
+}
+    document.getElementById("colorfdc437").addEventListener("click", changeColorfdc437);
+
+    const changeColor0c6834 = () => {
+        document.getElementById("lorem").style.color="#0c6834"
+    }
+        document.getElementById("color0c6834").addEventListener("click", changeColor0c6834);
+
+        const changeColor4a9ccf = () => {
+            document.getElementById("lorem").style.color="#4a9ccf"
+        }
+            document.getElementById("color4a9ccf").addEventListener("click", changeColor4a9ccf);
+
+            const changeColor661f45 = () => {
+                document.getElementById("lorem").style.color="#661f45"
+            }
+                document.getElementById("color661f45").addEventListener("click", changeColor661f45);
+
+                const changeColorf43e3d = () => {
+                    document.getElementById("lorem").style.color="#f43e3d"
+                }
+                    document.getElementById("colorf43e3d").addEventListener("click", changeColorf43e3d);
+    
+
